f0f471dfda34f90fb9c722385b2fe86f55adc48f
Никита Кашин <kashin.nik008@yandex.ru>
2021-04-20 10:05:21 +0300

    Добавьте файлы проекта.

---
+++ phinx/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace phinx
+{
+	public partial class Form1 : Form
+	{
+		MySqlConnection conn = new MySqlConnection("server=127.0.0.1;userid=root;password=root;database=phinx_db;sslMode=none");
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				conn.Open();
+
+				MySqlDataAdapter SDA = new MySqlDataAdapter("SELECT * FROM phinxlog", conn);
+				DataTable DATA = new DataTable();
+				SDA.Fill(DATA);
+				dataGridView1.DataSource = DATA;
+				conn.Close();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			finally
+			{
+				conn.Close();
+			}
+		}
+	}
+}
