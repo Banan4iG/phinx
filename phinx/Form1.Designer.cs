c2d8528b710021130a6234fe780bba4d6739a032
Никита Кашин <kashin.nik008@yandex.ru>
2021-04-22 16:32:22 +0300

    Список миграций

--- phinx/Form1.Designer.cs
+++ phinx/Form1.Designer.cs
@@ -28,35 +28,184 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.dataGridView1 = new System.Windows.Forms.DataGridView();
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// dataGridView1
+			// textBox1
 			// 
-			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Location = new System.Drawing.Point(12, 12);
-			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-			this.dataGridView1.TabIndex = 0;
+			this.textBox1.Location = new System.Drawing.Point(6, 19);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(218, 20);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(6, 59);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(111, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Создать миграцию";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(6, 112);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Применить";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(111, 112);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "Откатить";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.listView1);
+			this.groupBox1.Controls.Add(this.textBox1);
+			this.groupBox1.Controls.Add(this.button3);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.button2);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(362, 426);
+			this.groupBox1.TabIndex = 5;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Миграции";
+			// 
+			// listView1
+			// 
+			this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+			this.listView1.FullRowSelect = true;
+			this.listView1.GridLines = true;
+			this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+			this.listView1.HideSelection = false;
+			this.listView1.Location = new System.Drawing.Point(6, 161);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(350, 243);
+			this.listView1.TabIndex = 0;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Миграция";
+			this.columnHeader1.Width = 242;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Статус";
+			this.columnHeader2.Width = 100;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.button5);
+			this.groupBox2.Controls.Add(this.button4);
+			this.groupBox2.Controls.Add(this.textBox2);
+			this.groupBox2.Controls.Add(this.comboBox1);
+			this.groupBox2.Location = new System.Drawing.Point(380, 12);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(408, 426);
+			this.groupBox2.TabIndex = 6;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Редактирование";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(6, 19);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(234, 21);
+			this.comboBox1.TabIndex = 0;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(6, 46);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(396, 374);
+			this.textBox2.TabIndex = 1;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(246, 17);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 2;
+			this.button4.Text = "Открыть";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(327, 17);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 3;
+			this.button5.Text = "Сохранить";
+			this.button5.UseVisualStyleBackColor = true;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
-
-		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.ComboBox comboBox1;
 	}
 }
 
--- phinx/Form1.cs
+++ phinx/Form1.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
+using System.IO;
+using System.Data.SqlClient;
 
 namespace phinx
 {
@@ -17,18 +20,22 @@ namespace phinx
 		public Form1()
 		{
 			InitializeComponent();
+			MigView();
 		}
-
+		string main_path = @"cd e:\OpenServer\domains\localhost\phinx\";
+		string migrate = @"php vendor\robmorgan\phinx\bin\phinx migrate";
+		string rollback = @"php vendor\robmorgan\phinx\bin\phinx rollback";
+		string create = @"php vendor\robmorgan\phinx\bin\phinx create";
 		private void Form1_Load(object sender, EventArgs e)
 		{
 			try
 			{
 				conn.Open();
 
-				MySqlDataAdapter SDA = new MySqlDataAdapter("SELECT * FROM phinxlog", conn);
+				MySqlDataAdapter SDA = new MySqlDataAdapter("SELECT version, migration_name FROM phinxlog", conn);
 				DataTable DATA = new DataTable();
 				SDA.Fill(DATA);
-				dataGridView1.DataSource = DATA;
+				//dataGridView1.DataSource = DATA;
 				conn.Close();
 			}
 			catch (Exception ex)
@@ -39,6 +46,82 @@ namespace phinx
 			{
 				conn.Close();
 			}
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			Process.Start("CMD.exe", "/K " + main_path + " & " + rollback);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			Process.Start("CMD.exe", "/K " + main_path + " & " + migrate);
+		}
+
+		private void MigView()
+		{
+			string pathToMigrates = @"E:\OpenServer\domains\localhost\phinx\migrations";
+			List<string> listfiles = (from a in Directory.GetFiles(pathToMigrates) select Path.GetFileName(a)).ToList();
+			List<string> numberMigFromDir = new List<string>();
+			List<string> numberMigFromTable = new List<string>();
+			foreach (var el in listfiles)
+			{
+				string[] splitstr = el.Split('_');
+				numberMigFromDir.Add(splitstr[0]);
+ 			}
+			conn.Open();
+			MySqlCommand command = new MySqlCommand("SELECT version FROM phinxlog", conn);
+			MySqlDataReader reader = command.ExecuteReader();
+			while(reader.Read())
+			{
+				numberMigFromTable.Add(reader[0].ToString());
+			}
+			numberMigFromDir.Sort();
+			numberMigFromTable.Sort();
+			conn.Close();
+			for (int i = 0; i < numberMigFromDir.Count; i++)
+			{
+				try
+				{
+					if(numberMigFromDir[i] == numberMigFromTable[i])
+					{
+						conn.Open();
+						string sql = "SELECT migration_name FROM phinxlog WHERE version=" + numberMigFromTable[i];
+						MySqlCommand command1 = new MySqlCommand(sql, conn);
+						MySqlDataReader reader1 = command1.ExecuteReader();
+						while (reader1.Read())
+						{
+							string name = reader1[0].ToString();
+							listView1.Items.Add(name);
+							listView1.Items[listView1.Items.Count - 1].SubItems.Add("Done");
+						}
+						conn.Close();
+					}
+				}
+				catch
+				{
+					//MySqlCommand command1 = new MySqlCommand("SELECT migration_name FROM phinxlog WHERE version='" + numberMigFromTable + "'", conn);
+					//MySqlDataReader reader1 = command.ExecuteReader();
+					//string name = reader[0].ToString();
+					listView1.Items.Add("№" + numberMigFromDir[i]);
+					listView1.Items[listView1.Items.Count - 1].SubItems.Add("None");
+				}
+			}
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
