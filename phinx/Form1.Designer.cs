ee0670316007ed579b561ed612442be731db1868
Никита Кашин <kashin.nik008@yandex.ru>
2021-05-05 11:50:37 +0300

    Осталось: вывод статуса

--- phinx/Form1.Designer.cs
+++ phinx/Form1.Designer.cs
@@ -33,31 +33,36 @@
 			this.button2 = new System.Windows.Forms.Button();
 			this.button3 = new System.Windows.Forms.Button();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.listView1 = new System.Windows.Forms.ListView();
-			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
 			this.groupBox2 = new System.Windows.Forms.GroupBox();
-			this.comboBox1 = new System.Windows.Forms.ComboBox();
-			this.textBox2 = new System.Windows.Forms.TextBox();
-			this.button4 = new System.Windows.Forms.Button();
 			this.button5 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.опцииToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.проверитьПодключениеКБазеДанныхToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.открытьПапкуСМиграциямиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.открытьКонфигурационныйФайлPhinxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.groupBox1.SuspendLayout();
 			this.groupBox2.SuspendLayout();
+			this.menuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// textBox1
 			// 
 			this.textBox1.Location = new System.Drawing.Point(6, 19);
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(218, 20);
+			this.textBox1.Size = new System.Drawing.Size(233, 20);
 			this.textBox1.TabIndex = 1;
 			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(6, 59);
+			this.button1.Location = new System.Drawing.Point(453, 17);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(111, 23);
+			this.button1.Size = new System.Drawing.Size(112, 23);
 			this.button1.TabIndex = 2;
 			this.button1.Text = "Создать миграцию";
 			this.button1.UseVisualStyleBackColor = true;
@@ -65,7 +70,7 @@
 			// 
 			// button2
 			// 
-			this.button2.Location = new System.Drawing.Point(6, 112);
+			this.button2.Location = new System.Drawing.Point(6, 45);
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(75, 23);
 			this.button2.TabIndex = 3;
@@ -75,7 +80,7 @@
 			// 
 			// button3
 			// 
-			this.button3.Location = new System.Drawing.Point(111, 112);
+			this.button3.Location = new System.Drawing.Point(87, 45);
 			this.button3.Name = "button3";
 			this.button3.Size = new System.Drawing.Size(75, 23);
 			this.button3.TabIndex = 4;
@@ -85,46 +90,42 @@
 			// 
 			// groupBox1
 			// 
-			this.groupBox1.Controls.Add(this.listView1);
+			this.groupBox1.Controls.Add(this.checkBox1);
+			this.groupBox1.Controls.Add(this.textBox3);
 			this.groupBox1.Controls.Add(this.textBox1);
 			this.groupBox1.Controls.Add(this.button3);
 			this.groupBox1.Controls.Add(this.button1);
 			this.groupBox1.Controls.Add(this.button2);
-			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Location = new System.Drawing.Point(12, 27);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(362, 426);
+			this.groupBox1.Size = new System.Drawing.Size(571, 426);
 			this.groupBox1.TabIndex = 5;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Миграции";
 			// 
-			// listView1
+			// checkBox1
 			// 
-			this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
-            this.columnHeader2});
-			this.listView1.FullRowSelect = true;
-			this.listView1.GridLines = true;
-			this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-			this.listView1.HideSelection = false;
-			this.listView1.Location = new System.Drawing.Point(6, 161);
-			this.listView1.Name = "listView1";
-			this.listView1.Size = new System.Drawing.Size(350, 243);
-			this.listView1.TabIndex = 0;
-			this.listView1.UseCompatibleStateImageBehavior = false;
-			this.listView1.View = System.Windows.Forms.View.Details;
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Checked = true;
+			this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox1.Location = new System.Drawing.Point(245, 21);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(208, 17);
+			this.checkBox1.TabIndex = 6;
+			this.checkBox1.Text = "Открыть файл созданной миграции";
+			this.checkBox1.UseVisualStyleBackColor = true;
 			// 
-			// columnHeader1
+			// textBox3
 			// 
-			this.columnHeader1.Text = "Миграция";
-			this.columnHeader1.Width = 242;
-			// 
-			// columnHeader2
-			// 
-			this.columnHeader2.Text = "Статус";
-			this.columnHeader2.Width = 100;
+			this.textBox3.AcceptsReturn = true;
+			this.textBox3.AcceptsTab = true;
+			this.textBox3.Font = new System.Drawing.Font("AcadEref", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox3.Location = new System.Drawing.Point(6, 74);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.ReadOnly = true;
+			this.textBox3.Size = new System.Drawing.Size(559, 343);
+			this.textBox3.TabIndex = 5;
 			// 
 			// groupBox2
 			// 
@@ -132,38 +133,12 @@
 			this.groupBox2.Controls.Add(this.button4);
 			this.groupBox2.Controls.Add(this.textBox2);
 			this.groupBox2.Controls.Add(this.comboBox1);
-			this.groupBox2.Location = new System.Drawing.Point(380, 12);
+			this.groupBox2.Location = new System.Drawing.Point(589, 27);
 			this.groupBox2.Name = "groupBox2";
 			this.groupBox2.Size = new System.Drawing.Size(408, 426);
 			this.groupBox2.TabIndex = 6;
 			this.groupBox2.TabStop = false;
 			this.groupBox2.Text = "Редактирование";
-			// 
-			// comboBox1
-			// 
-			this.comboBox1.FormattingEnabled = true;
-			this.comboBox1.Location = new System.Drawing.Point(6, 19);
-			this.comboBox1.Name = "comboBox1";
-			this.comboBox1.Size = new System.Drawing.Size(234, 21);
-			this.comboBox1.TabIndex = 0;
-			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-			// 
-			// textBox2
-			// 
-			this.textBox2.Location = new System.Drawing.Point(6, 46);
-			this.textBox2.Multiline = true;
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(396, 374);
-			this.textBox2.TabIndex = 1;
-			// 
-			// button4
-			// 
-			this.button4.Location = new System.Drawing.Point(246, 17);
-			this.button4.Name = "button4";
-			this.button4.Size = new System.Drawing.Size(75, 23);
-			this.button4.TabIndex = 2;
-			this.button4.Text = "Открыть";
-			this.button4.UseVisualStyleBackColor = true;
 			// 
 			// button5
 			// 
@@ -174,13 +149,83 @@
 			this.button5.Text = "Сохранить";
 			this.button5.UseVisualStyleBackColor = true;
 			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(246, 17);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 2;
+			this.button4.Text = "Открыть";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(6, 46);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(396, 374);
+			this.textBox2.TabIndex = 1;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(6, 19);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(234, 21);
+			this.comboBox1.TabIndex = 0;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.опцииToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(1002, 24);
+			this.menuStrip1.TabIndex = 7;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// опцииToolStripMenuItem
+			// 
+			this.опцииToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.проверитьПодключениеКБазеДанныхToolStripMenuItem,
+            this.открытьПапкуСМиграциямиToolStripMenuItem,
+            this.открытьКонфигурационныйФайлPhinxToolStripMenuItem});
+			this.опцииToolStripMenuItem.Name = "опцииToolStripMenuItem";
+			this.опцииToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+			this.опцииToolStripMenuItem.Text = "Опции";
+			// 
+			// проверитьПодключениеКБазеДанныхToolStripMenuItem
+			// 
+			this.проверитьПодключениеКБазеДанныхToolStripMenuItem.Name = "проверитьПодключениеКБазеДанныхToolStripMenuItem";
+			this.проверитьПодключениеКБазеДанныхToolStripMenuItem.Size = new System.Drawing.Size(299, 22);
+			this.проверитьПодключениеКБазеДанныхToolStripMenuItem.Text = "Проверить подключение к базе данных";
+			this.проверитьПодключениеКБазеДанныхToolStripMenuItem.Click += new System.EventHandler(this.проверитьПодключениеКБазеДанныхToolStripMenuItem_Click);
+			// 
+			// открытьПапкуСМиграциямиToolStripMenuItem
+			// 
+			this.открытьПапкуСМиграциямиToolStripMenuItem.Name = "открытьПапкуСМиграциямиToolStripMenuItem";
+			this.открытьПапкуСМиграциямиToolStripMenuItem.Size = new System.Drawing.Size(299, 22);
+			this.открытьПапкуСМиграциямиToolStripMenuItem.Text = "Открыть папку с миграциями";
+			this.открытьПапкуСМиграциямиToolStripMenuItem.Click += new System.EventHandler(this.открытьПапкуСМиграциямиToolStripMenuItem_Click);
+			// 
+			// открытьКонфигурационныйФайлPhinxToolStripMenuItem
+			// 
+			this.открытьКонфигурационныйФайлPhinxToolStripMenuItem.Name = "открытьКонфигурационныйФайлPhinxToolStripMenuItem";
+			this.открытьКонфигурационныйФайлPhinxToolStripMenuItem.Size = new System.Drawing.Size(299, 22);
+			this.открытьКонфигурационныйФайлPhinxToolStripMenuItem.Text = "Открыть конфигурационный файл Phinx";
+			this.открытьКонфигурационныйФайлPhinxToolStripMenuItem.Click += new System.EventHandler(this.открытьКонфигурационныйФайлPhinxToolStripMenuItem_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(1002, 457);
 			this.Controls.Add(this.groupBox2);
 			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
@@ -188,7 +233,10 @@
 			this.groupBox1.PerformLayout();
 			this.groupBox2.ResumeLayout(false);
 			this.groupBox2.PerformLayout();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -198,14 +246,18 @@
 		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.Button button3;
 		private System.Windows.Forms.GroupBox groupBox1;
-		private System.Windows.Forms.ListView listView1;
-		private System.Windows.Forms.ColumnHeader columnHeader1;
-		private System.Windows.Forms.ColumnHeader columnHeader2;
 		private System.Windows.Forms.GroupBox groupBox2;
 		private System.Windows.Forms.Button button5;
 		private System.Windows.Forms.Button button4;
 		private System.Windows.Forms.TextBox textBox2;
 		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem опцииToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem проверитьПодключениеКБазеДанныхToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem открытьПапкуСМиграциямиToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem открытьКонфигурационныйФайлPhinxToolStripMenuItem;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.CheckBox checkBox1;
 	}
 }
 
--- phinx/Form1.cs
+++ phinx/Form1.cs
@@ -11,41 +11,36 @@ using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 using System.IO;
 using System.Data.SqlClient;
+using System.Threading;
+using System.Text.RegularExpressions;
 
 namespace phinx
 {
 	public partial class Form1 : Form
 	{
-		MySqlConnection conn = new MySqlConnection("server=127.0.0.1;userid=root;password=root;database=phinx_db;sslMode=none");
-		public Form1()
-		{
-			InitializeComponent();
-			MigView();
-		}
+		MySqlConnection conn;
+
 		string main_path = @"cd e:\OpenServer\domains\localhost\phinx\";
 		string migrate = @"php vendor\robmorgan\phinx\bin\phinx migrate";
 		string rollback = @"php vendor\robmorgan\phinx\bin\phinx rollback";
-		string create = @"php vendor\robmorgan\phinx\bin\phinx create";
+		string create = @"php vendor\robmorgan\phinx\bin\phinx create ";
+		string status = @"php vendor\robmorgan\phinx\bin\phinx status";
+		Dictionary<string, string> pathDictionary;
+
+		public Form1()
+		{
+			conn = new MySqlConnection("server=127.0.0.1;userid=root;password=root;database=phinx_db;sslMode=none");
+			InitializeComponent();
+		}
+		
 		private void Form1_Load(object sender, EventArgs e)
 		{
-			try
+			if(СheckConnection(conn))
 			{
-				conn.Open();
-
-				MySqlDataAdapter SDA = new MySqlDataAdapter("SELECT version, migration_name FROM phinxlog", conn);
-				DataTable DATA = new DataTable();
-				SDA.Fill(DATA);
-				//dataGridView1.DataSource = DATA;
-				conn.Close();
+				//MigView();
+				
 			}
-			catch (Exception ex)
-			{
-				MessageBox.Show(ex.Message);
-			}
-			finally
-			{
-				conn.Close();
-			}
+			ComboBoxAddItems();
 		}
 
 		private void textBox1_TextChanged(object sender, EventArgs e)
@@ -55,21 +50,105 @@ namespace phinx
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-
+			Process p = new Process();
+			ProcessStartInfo startInfo = new ProcessStartInfo();
+			startInfo.UseShellExecute = false;
+			startInfo.RedirectStandardOutput = true;
+			startInfo.FileName = "CMD.exe";
+			startInfo.Arguments = "/C " + main_path + " & " + create + textBox1.Text;
+			p.StartInfo = startInfo;
+			p.Start();
+			p.Close();
+			Thread.Sleep(500);
+			if(checkBox1.Checked == true)
+			{
+				string[] listMig = Directory.GetFiles(@"E:\OpenServer\domains\localhost\phinx\migrations");
+				int lastEl = listMig.Length;
+				Process.Start(listMig[lastEl-1]);
+			}
+			ComboBoxAddItems();
 		}
 
 		private void button3_Click(object sender, EventArgs e)
 		{
-			Process.Start("CMD.exe", "/K " + main_path + " & " + rollback);
+			Process p = new Process();
+			ProcessStartInfo startInfo = new ProcessStartInfo();
+			startInfo.UseShellExecute = false;
+			startInfo.RedirectStandardOutput = true;
+			startInfo.FileName = "CMD.exe";
+			startInfo.Arguments = "/C " + main_path + " & " + rollback + " & exit";
+			p.StartInfo = startInfo;
+			p.Start();
+			p.Close();
 		}
 
 		private void button2_Click(object sender, EventArgs e)
 		{
-			Process.Start("CMD.exe", "/K " + main_path + " & " + migrate);
+			Process p = new Process();
+			ProcessStartInfo startInfo = new ProcessStartInfo();
+			startInfo.UseShellExecute = false;
+			startInfo.RedirectStandardOutput = true;
+			startInfo.FileName = "CMD.exe";
+			startInfo.Arguments = "/C " + main_path + " & " + migrate + " & exit";
+			p.StartInfo = startInfo;
+			p.Start();
+			p.Close();
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void проверитьПодключениеКБазеДанныхToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				conn.Open();
+				MessageBox.Show(this, "Подлючение есть!", "Проверка подлючения к базе данных");
+			}
+			catch
+			{
+				MessageBox.Show(this, "Подлючение отсутствует!", "Проверка подлючения к базе данных");
+			}
+			finally
+			{
+				conn.Close();
+			}
+		}
+
+		private void открытьПапкуСМиграциямиToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Process.Start(@"E:\OpenServer\domains\localhost\phinx\migrations");
+
+			/*
+			Process p = new Process();
+			ProcessStartInfo startInfo = new ProcessStartInfo();
+			startInfo.UseShellExecute = false;
+			startInfo.RedirectStandardOutput = true;
+			startInfo.FileName = "explorer.exe";
+			startInfo.Arguments = @"E:\OpenServer\domains\localhost\phinx\migrations";
+			p.StartInfo = startInfo;
+			p.Start();
+			p.Close();
+			*/
+		}
+
+		private void открытьКонфигурационныйФайлPhinxToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Process.Start(@"E:\OpenServer\domains\localhost\phinx\phinx.yml");
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			string selected = comboBox1.SelectedItem.ToString();
+			Process.Start(@"E:\OpenServer\domains\localhost\phinx\migrations\" + pathDictionary[selected]);
 		}
 
 		private void MigView()
 		{
+			/*
+			listView1.Items.Clear();
 			string pathToMigrates = @"E:\OpenServer\domains\localhost\phinx\migrations";
 			List<string> listfiles = (from a in Directory.GetFiles(pathToMigrates) select Path.GetFileName(a)).ToList();
 			List<string> numberMigFromDir = new List<string>();
@@ -116,12 +195,67 @@ namespace phinx
 					listView1.Items.Add("№" + numberMigFromDir[i]);
 					listView1.Items[listView1.Items.Count - 1].SubItems.Add("None");
 				}
+			}*/
+
+			Process p = new Process();
+			ProcessStartInfo startInfo = new ProcessStartInfo();
+			startInfo.UseShellExecute = false;
+			startInfo.RedirectStandardOutput = true;
+			startInfo.FileName = "CMD.exe";
+			startInfo.Arguments = "/C " + main_path + " & " + status + " & exit";
+			p.StartInfo = startInfo;
+			p.Start();
+			StreamReader reader = p.StandardOutput;
+			string reads = reader.ReadToEnd();
+			p.Close();
+
+			string pattern = @"(?=Status\s)([\s\S]+)(?=\r\n\r\n)";
+			RegexOptions option = RegexOptions.Multiline;
+			textBox3.Clear();
+			string result = "";
+			foreach (Match m in Regex.Matches(reads, pattern, option))
+			{
+				result += m.Value;
+				textBox3.Text += result;
 			}
 		}
 
-		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		private bool СheckConnection(MySqlConnection conn)
 		{
+			try
+			{
+				conn.Open();
+				return true;
+			}
+			catch
+			{
+				MessageBox.Show(this, "Подлючение отсутствует!", "Проверка подлючения к базе данных");
+				return false;
+			}
+			finally
+			{
+				conn.Close();
+			}
+		}
 
+		private void ComboBoxAddItems()
+		{
+			comboBox1.Items.Clear();
+			pathDictionary = new Dictionary<string, string>();
+			string pathToMigrates = @"E:\OpenServer\domains\localhost\phinx\migrations";
+			List<string> listFiles = (from a in Directory.GetFiles(pathToMigrates) select Path.GetFileName(a)).ToList();
+			foreach(var path in listFiles)
+			{
+				string[] splitedName = path.Split('_');
+				string dictionaryKey = "";
+				for (int i = 1; i < splitedName.Length; i++)
+				{
+					dictionaryKey += splitedName[i].ToUpper()[0] + splitedName[i].Substring(1);
+				}
+				string trimedDictionaryKey = dictionaryKey.Remove(dictionaryKey.Length - 4);
+				pathDictionary.Add(trimedDictionaryKey, path);
+				comboBox1.Items.Add(trimedDictionaryKey);
+			}
 		}
 	}
 }
